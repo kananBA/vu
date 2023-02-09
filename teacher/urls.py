872d70961af95343c2316d59b8309f8a09f9040a
Kanan <kanan.bagheri@gmail.com>
2023-02-09 07:58:27 +0000

    Modify teacher app

--- teacher/urls.py
+++ teacher/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import DashboardTemplateView
+from .views import DashboardTemplateView, CourseTemplateView, CourseNoticeTemplateView, TaskTemplateView
 
 app_name = 'teacher'
 urlpatterns = [
     path('dashboard/', DashboardTemplateView.as_view(), name='index'),
+    path('course/<int:pk>/', CourseTemplateView.as_view(), name='course'),
+    path('notice/<int:pk>/', CourseNoticeTemplateView.as_view(), name='notice'),
+    path('task/<int:pk>/', TaskTemplateView.as_view(), name='notice'),
 ]
--- teacher/views.py
+++ teacher/views.py
@@ -1,9 +1,37 @@
 from django import views
 from django.shortcuts import render
 
+from course.models import Course
+
 # Create your views here.
 
 
 class DashboardTemplateView(views.View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'teacher/dashboard.html', {})
+        teacher = self.request.user
+
+        course_list = Course.objects.filter(creator=teacher)
+
+        data = {
+            'course_list': course_list,
+        }
+
+        return render(request, 'teacher/dashboard.html', data)
+
+class CourseTemplateView(views.View):
+    def get(self, request, pk=None, *args, **kwargs):
+        course = Course.objects.get(id=pk)
+
+        data = {
+            'course': course,
+        }
+
+        return render(request, 'teacher/course.html', data)
+
+class CourseNoticeTemplateView(views.View):
+    def get(self, request, pk=None, *args, **kwargs):
+        return render(request, 'teacher/notice.html', {})
+
+class TaskTemplateView(views.View):
+    def get(self, request, pk=None, *args, **kwargs):
+        return render(request, 'teacher/task.html', {})
