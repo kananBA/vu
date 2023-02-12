224f26593f0d848b37d574272313ee198c5fadac
Kanan <kanan.bagheri@gmail.com>
2023-02-12 11:56:57 +0000

    Create notice.html in student app

--- student/urls.py
+++ student/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import DashboardTemplateView
+from .views import DashboardTemplateView, NoticeTemplateView
 
 app_name = 'student'
 urlpatterns = [
     path('dashboard/', DashboardTemplateView.as_view(), name='index'),
+    path('notice/<int:pk>/', NoticeTemplateView.as_view(), name='index'),
 ]
--- student/views.py
+++ student/views.py
@@ -1,9 +1,23 @@
 from django import views
 from django.shortcuts import render
 
+from course.models import Course, CourseNotice
+
 # Create your views here.
 
 
 class DashboardTemplateView(views.View):
     def get(self, request, *args, **kwargs):
         return render(request, 'student/dashboard.html', {})
+
+
+class NoticeTemplateView(views.View):
+    def get(self, request, pk=None, *args, **kwargs):
+        course = Course.objects.get(id=pk)
+        course_notice_list = CourseNotice.objects.filter(course=course)
+
+        data = {
+            'course_notice_list': course_notice_list,
+        }
+
+        return render(request, 'student/notice.html', data)
